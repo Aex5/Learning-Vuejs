e643d6d91f6a71b03bcc7d3de238a810379eb75d
Sujaruu <ytfuga02@gmail.com>
2022-11-02 21:33:10 +0700

    up code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <a v-bind:href="url">Go To Link</a><br>
   <input type="text" v-model="nama">
-  {{nama}}
+  {{nama}} <br>
+
+<div v-if="products.length"> 
+  <ul>
+    <li v-for="item in products" :key="item.id">{{item.id}} - {{item.model}} - {{item.price}}</li>
+  </ul>
+</div>
+
+<div v-else>
+  <p>Product Not Found</p>
+</div>
+ 
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
   data() {
     return {
       url: "https://www.google.com",
-      nama: "adit"
+      nama: "adit",
+      products: [
+        {id:1, model: "pc", price: 10000},
+        {id:2, model: "pc", price: 20000},
+        {id:3, model: "pc", price: 30000},
+        {id:4, model: "pc", price: 40000},
+        {id:5, model: "pc", price: 50000},
+      ]
     }
   }
 }
